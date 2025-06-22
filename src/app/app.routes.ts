c9cee69559c2bb3c51c321ddcfb7fc9401db3913
babacamara91 <babacamara91@gmail.com>
2025-06-22 10:31:39 +0200

    int layout et carousel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {Home} from './features/home/home/home';
+import {Layout} from './core/layout/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+    ]
+  }
+];
---
+++ src/app/features/home/components/carousel/carousel.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {
+  CarouselCaptionComponent,
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent
+}from '@coreui/angular';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-carousel',
+  imports: [
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    CarouselCaptionComponent,
+    CarouselControlComponent,
+  ],
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.scss'
+})
+export class Carousel {
+
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+  constructor() {
+    this.slides[0] = {
+      id: 0,
+      src: 'assets/slides/slide1.jpg',
+      title: 'Réduisez votre empreinte carbone',
+      subtitle: 'Voyagez de maniére écologique avec EcoRide.'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: 'assets/slides/slide2.jpg',
+      title: 'Economiser sur vos trajets',
+      subtitle: 'Des prix avantageux pour tous les trajets.',
+    };
+    this.slides[2] = {
+      id: 2,
+      src: 'assets/slides/slide3.jpg',
+      title: 'Confort et Sécurité',
+      subtitle: 'Avec des conducteurs vérifiés et recommandés.',
+    };
+  }
+}
